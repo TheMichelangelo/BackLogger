f0db3a57149ab22348af9743096c039197fd6d82
TheMichelangelo <TheMichelangelo@users.noreply.github.com>
2023-06-18 21:55:12 +0300

    Initial Commit

---
+++ Backloger/BacklogerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BacklogerApp.swift
+//  Backloger
+//
+//  Created by Mike Pastula on 18.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct BacklogerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
