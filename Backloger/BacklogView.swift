0dd90adfc68329c9cefab017a526f982547f5005
TheMichelangelo <TheMichelangelo@users.noreply.github.com>
2023-06-19 23:24:54 +0300

    first commit

---
+++ Backloger/BacklogView.swift
@@ -0,0 +1,20 @@
+//
+//  BacklogView.swift
+//  Backloger
+//
+//  Created by Mike Pastula on 19.06.2023.
+//
+
+import SwiftUI
+
+struct BacklogView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BacklogView_Previews: PreviewProvider {
+    static var previews: some View {
+        BacklogView()
+    }
+}
